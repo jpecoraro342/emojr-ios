3336ac22146f7a6cda717fd412d98a53d89a308b
jpecoraro342 <jpecoraro342@ufl.edu>
2016-06-09 19:59:11 -0400

    uhhh emojis...

---
+++ Emojr/EmojiCell.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiCell.swift
+//  Emojr
+//
+//  Created by Joseph Pecoraro on 6/9/16.
+//  Copyright © 2016 Joseph Pecoraro. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCell: UICollectionViewCell {
+    
+}
