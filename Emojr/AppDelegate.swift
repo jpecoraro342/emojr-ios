9c802841e97b04b3e4baf4935261eea2bb67faf6
James Risberg <jimmyrisberg56@gmail.com>
2017-02-26 16:35:45 -0800

    Add Firebase and Google sign in using cocoa pods

--- Emojr/AppDelegate.swift
+++ Emojr/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Fabric
 import Crashlytics
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         Fabric.with([Crashlytics.self])
+        
+        FIRApp.configure()
         
         return true
     }
---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Emojr' do
+  use_frameworks!
+
+  # Pods for Emojr
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'GoogleSignIn'
+
+  target 'EmojrTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
