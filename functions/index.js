38c22830154dc22d07f9dfdd8b990249f505a2dd
jpecoraro342 <jpecoraro342@ufl.edu>
2017-05-03 00:27:17 -0400

    Removed node modules, updated onFollow function.

--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,38 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+admin.initializeApp(functions.config().firebase);
 
+exports.onFollow = functions.database.ref('/follows/{followedUser}').onWrite(event => {
+	let original = event.data.previous.val();
+	let current = event.data.current.val();
 
-exports.onFollow = functions.database.ref('/follows/{followedUser}')
-	.onWrite(event => {
+	original = original ? Object.keys(original) : [];
+	current = current ? Object.keys(current) : [];
 
-	const original = Object.keys(event.data.previous.val());
-	const current = Object.keys(event.data.val());
+	const newFollows = current.filter(follow => original.indexOf(follow) < 0);
+	const removedFollow = original.filter(follow => current.indexOf(follow) < 0);
 
-	const followed = (original.count < current.count);
+	console.log('New Follow: ', newFollows);
+	console.log('Removed Follow: ', removedFollow);
 
-	const difference = original.filter(function(i) { return current.indexOf(i) < 0; });
+	if (newFollows.length != 1) { 
+		console.log(`needed 1 new follow to add timeline, found ${newFollows.length}`);
+		return Promise.resolve(); 
+	}
 
-	if (difference.length != 1) { console.log("There should only be one..."); return Promise.resolve(); }
-	const newFollower = difference[0];
-
-	let timelineRef = functions.database.ref().child('timeline').child(newFollower);
+	let timelineRef = admin.database().ref().child('timeline').child(event.params.followedUser);
+	let timelinePosts = {};
 
 	return timelineRef.once('value')
-	  .then(function(timelineSnapshot) {
-		  	let userRef = functions.database.ref().child('userPosts').child(event.params.followedUser).once('value')
-				  .then(function(userSnapchat) {
+	  .then(timelineSnapshot => {
+	  	timelinePosts = timelineSnapshot.val() || {};
+		return admin.database().ref().child('userPosts').child(newFollows[0]).once('value')
+	  })
+	  .then(userSnapchat => {
+	  	let userPosts = userSnapchat.val() || {};
+	  	timelinePosts = Object.assign(timelinePosts, userPosts);
 
-				   const timelinePosts = timelineSnapshot.val()
-				   const userPosts = userSnapchat.val()
-
-				   if (timelinePosts.constructor !== Array) { console.log("timelinePosts was not array"); return; }
-				   if (userPosts.constructor !== Array) { console.log("userPosts was not array"); return; }
-
-				   if (followed) {
-				   		timelinePosts.push(...userPosts);
-				   } else {
-				   		timelinePosts = timelinePosts.filter(function(i) { return userPosts.indexOf(i) < 0; });
-				   }
-				   
-				   timelineRef.update(timelinePosts);
-			 });
-		});
+	  	return timelineRef.update(timelinePosts);
+	 });
 });
