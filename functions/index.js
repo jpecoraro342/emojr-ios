16cb0c799c2066d6a2962736f3f60c8ab00c40b2
jpecoraro342 <jpecoraro342@ufl.edu>
2018-04-27 13:53:06 -0400

    Updated firebase follow and unfollow functions.

--- functions/index.js
+++ functions/index.js
@@ -3,46 +3,93 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+// When a user follows another user, add those users posts to their timeline
 exports.onFollow = functions.database.ref('/follows/{followedUser}').onCreate((snapshot, context) => {	
 	const createdData = snapshot.val();
 	
-	console.log('Created:\n' + createdData);
-	console.log('UID: ' + EventContext.auth.uid);
+	console.log('Created: ');
+	console.log(createdData);
 
-	let timelineRef = admin.database().ref().child('timeline').child(EventContext.auth.uid);
+	const createdId = Object.keys(createdData)[0];
+
+	console.log('CreatedId: ');
+	console.log(createdId);
+
+	const userId = context.auth.uid;
+	console.log('UserId: ');
+	console.log(userId);
+
+	// Get the timeline for the user
+	let timelineRef = admin.database().ref().child('timeline').child(userId);
 	let timelinePosts = {};
 
 	return timelineRef.once('value')
 	  .then(timelineSnapshot => {
+	  	// Get the posts
 	  	timelinePosts = timelineSnapshot.val() || {};
+
+	  	console.log('Posts: ');
+	  	console.log(timelinePosts);
 	  	
-		return admin.database().ref().child('userPosts').child(createdData).once('value')
+		return admin.database().ref().child('userPosts').child(createdId).once('value')
 	  })
 	  .then(userSnapchat => {
+	  	// Add all of the new users posts to the timeline
 		let userPosts = userSnapchat.val() || {};
 		timelinePosts = Object.assign(timelinePosts, userPosts);
+
+		console.log('Posts after Add: ');
+	  	console.log(timelinePosts);
 
 	  	return timelineRef.update(timelinePosts);
 	 });
 });
 
+// When a user stops following another user, remove those users posts from their timeline
 exports.onUnfollow = functions.database.ref('/follows/{followedUser}').onDelete((snapshot, context) => {	
 	const deletedData = snapshot.val();
 	
-	console.log('Deleted:\n' + deletedData);
-	console.log('UID: ' + EventContext.auth.uid);
+	console.log('Deleted: ');
+	console.log(deletedData);
 
-	let timelineRef = admin.database().ref().child('timeline').child(EventContext.auth.uid);
+	const deletedId = Object.keys(deletedData)[0];
+
+	console.log('DeletedId:');
+	console.log(deletedId);
+
+	const userId = context.auth.uid;
+	console.log('UserId: ');
+	console.log(userId);
+
+	// Get the timeline for the user
+	let timelineRef = admin.database().ref().child('timeline').child(userId);
 	let timelinePosts = {};
 
 	return timelineRef.once('value')
 	  .then(timelineSnapshot => {
 	  	timelinePosts = timelineSnapshot.val() || {};
 	  	
-	  	console.log('Posts:\n' + timelinePosts);
+	  	console.log('Posts: ');
+	  	console.log(timelinePosts);
+
+	  	let updatedPosts = {};
+
+	  	console.log(Object.keys(timelinePosts));
 	  	
-	  	// delete deletedData
+	  	// Iterate throught the timeline posts, removing or adding posts to the new array depending on if the post should be deleted or not
+	  	timelinePosts = Object.keys(timelinePosts).forEach(postId => {
+	  		let post = timelinePosts[postId];
+	  		if (post.userID !== deletedId) {
+	  			updatedPosts['' + postId] = post;
+	  		}
+	  		else {
+	  			updatedPosts['' + postId] = null;
+	  		}
+	  	});
+
+	  	console.log('Posts after Delete: ');
+	  	console.log(updatedPosts);
 	  	
-		return timelineRef.update(timelinePosts);
+		return timelineRef.update(updatedPosts);
 	  });
 });
